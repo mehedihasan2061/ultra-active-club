508d53df6fbff1d0a3659c4328b1de72ae571bae
mehedihasan2061 <mehedi.hasan206191@gmail.com>
2022-09-29 07:01:32 +0600

    nav added

---
+++ src/component/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+      <div>
+        <h4 className="fs-3 p-4 mb-2 text-primary">Daily Exercise Training</h4>
+        <h5 className="px-4">Select Today's Exercise</h5>
+      </div>
+    );
+};
+
+export default Header;
