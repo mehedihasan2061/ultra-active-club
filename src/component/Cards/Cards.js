27996611f10472f48c2965b388cc1f2b2d23ed6b
mehedihasan2061 <mehedi.hasan206191@gmail.com>
2022-09-29 12:47:37 +0600

    added text

--- src/component/Cards/Cards.js
+++ src/component/Cards/Cards.js
@@ -22,7 +22,47 @@ const Cards = () => {
             <p>
               <small>Mehedi Hasan</small>
             </p>
+            <p>
+              <small>Mymensingh,Bangladesh</small>
+            </p>
           </div>
+          <div className="personal-info">
+            <div>
+              <p>60kg</p>
+              <h6>Weight</h6>
+            </div>
+            <div>
+              <p>5.8</p>
+              <h6>Height</h6>
+            </div>
+            <div>
+              <p>22 Y</p>
+              <h6>Age</h6>
+            </div>
+          </div>
+          {/* add a break  */}
+          <div>
+            <h6 className="p-2 bg-primary text-white rounded my-3">
+              Add a Break
+            </h6>
+            <div className="timer">
+              <p className="time">10m</p>
+              <p className="time">15m</p>
+              <p className="time">20m</p>
+              <p className="time">30m</p>
+            </div>
+            <div></div>
+          </div>
+
+          <div>
+            <h5 className="p-2 bg-primary text-white rounded my-3">
+              Exercise Detail
+            </h5>
+            <small className="time-calc">Exercise time : 0 min</small>
+            <br />
+            <small className="time-calc">Break time : 0 min</small>
+          </div>
+          <button className="activity-btn">Activity Complete</button>
         </div>
       </div>
     );
