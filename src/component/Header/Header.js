4ef9863bb450a9ea621be92be4f25d08a7d716dd
mehedihasan2061 <mehedi.hasan206191@gmail.com>
2022-09-30 14:54:12 +0600

    favicon added

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,16 +1,17 @@
 import { faDumbbell } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import './Header.css'
 
 const Header = () => {
     return (
-      <div className='header'>
-        <span>
+      <div className='m-3'>
+        <p className="header container shadow py-3 bg-body rounded">
           <FontAwesomeIcon icon={faDumbbell}></FontAwesomeIcon>
-          <h4 className="container shadow p-3 m-5 bg-body rounded text-primary">
+          <h4 className="container rounded text-primary">
             Daily Exercise Training
           </h4>
-        </span>
+        </p>
         <h5 className="container px-4">Select Today's Exercise</h5>
       </div>
     );
