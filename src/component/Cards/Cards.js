af0b1ce132eef71f07b810b6df4e8cdff7c6009c
mehedihasan2061 <mehedi.hasan206191@gmail.com>
2022-09-30 10:09:58 +0600

    exercise time added

--- src/component/Cards/Cards.js
+++ src/component/Cards/Cards.js
@@ -2,23 +2,38 @@ import React, { useEffect, useState } from 'react';
 import Activity from '../Activity/Activity';
 import './Cards.css'
 
+
 const Cards = () => {
-    const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState([]);
+  const [detail,setDetail]=useState([])
     useEffect(() => {
         fetch('data.json')
             .then(res => res.json())
         .then(data=>setActivities(data))
-    },[])
+    }, [])
+  const addToList = (time) => {
+    console.log(time);
+   
+    const newDetail = [...detail, time];
+    setDetail(newDetail);
+  };
+  const totalTime = detail.reduce(
+    (partial, current) => partial + current,
+    0
+  );
     return (
       <div className="container">
         <div className="card-container">
           {activities.map((activity) => (
-            <Activity activity={activity} key="activity.id"></Activity>
+            <Activity key={activity.id}
+            addToList={addToList}
+              activity={activity}
+            ></Activity>
           ))}
         </div>
         <div className="info-container">
           <div>
-                <img src= "../../mh_rajon-removebg-preview.png" alt="" />
+            <img src="#" alt="" />
             <p>
               <small>Mehedi Hasan</small>
             </p>
@@ -45,8 +60,8 @@ const Cards = () => {
             <h6 className="p-2 bg-primary text-white shadow-lg rounded my-3">
               Add a Break
             </h6>
-            <div className="timer">            
-              <p className="time">10m</p>              
+            <div className="timer">
+              <p className="time">10m</p>
               <p className="time">15m</p>
               <p className="time">20m</p>
               <p className="time">30m</p>
@@ -58,7 +73,7 @@ const Cards = () => {
             <h5 className="p-2 bg-primary text-white rounded my-3">
               Exercise Detail
             </h5>
-            <small className="time-calc">Exercise time : 0 min</small>
+            <small className="time-calc">Exercise time : {totalTime} m</small>
             <br />
             <small className="time-calc">Break time : 0 min</small>
           </div>
